961e82c028077127ab851db80fa5dc1150aa8c8a
hyyou-muhayu <hyyou@muhayu.com>
2019-03-06 12:11:53 +0900

    first commit

---
+++ src/main/java/com/muhayu/hyyouchatbot/hyyouchatbot/HyyouChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.muhayu.hyyouchatbot.hyyouchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyyouChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyyouChatbotApplication.class, args);
+    }
+
+}
